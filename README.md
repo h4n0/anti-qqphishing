181f73a4dde1e9987283effe8ed61a8060186a77
zhao-han <windzhaohan@gmail.com>
2014-12-06 10:28:00 +0100

    first commit with scrapy

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ anti-qqphishing
 ===============
 
 This project will aim to defend from tons of fake QQ related phishing websites.
+
+I'm using [Scrapy] to initialize the web crawler framework.
+
+[Scrapy]:http://scrapy.org
---
+++ redspider/redspider/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for redspider project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'redspider'
+
+SPIDER_MODULES = ['redspider.spiders']
+NEWSPIDER_MODULE = 'redspider.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'redspider (+http://www.yourdomain.com)'
